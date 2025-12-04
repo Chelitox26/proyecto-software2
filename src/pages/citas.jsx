1604b4fed1ab805b49d933e1b95e5cb1ac436931
Chelitox26 <castilloalejandro2626@gmail.com>
2025-12-03 22:36:39 -0600

    update-css-citas

--- src/pages/citas.jsx
+++ src/pages/citas.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { FaUser, FaUsers, FaCalendarAlt, FaUserMd, FaFileInvoice, FaChartBar } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
-import "./citas.css";
+import "../App.css";
 
 export default function Citas() {
 
